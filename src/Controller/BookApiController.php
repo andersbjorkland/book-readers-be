5f59e05f80b1568230a35b5960f0c6208e7a36d1
Anders Björkland <git@andersbjorkland.online>
2021-01-09 09:24:13 +0100

    updated api to fetch single volume detail

--- src/Controller/BookApiController.php
+++ src/Controller/BookApiController.php
@@ -11,6 +11,21 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class BookApiController extends AbstractController
 {
     /**
+     * @Route("/api/details/{id}", name="book_details")
+     */
+    public function details(string $id, HttpClientInterface $client): Response
+    {
+        $apiToken = $this->getParameter('app.api_token');
+        $apiKeyValue = $apiToken ? "&key=" . $apiToken : "";
+
+        $response = $client->request(
+            'GET',
+            'https://www.googleapis.com/books/v1/volumes/' . $id . $apiKeyValue
+        );
+        return $this->json($response->toArray());
+    }
+
+    /**
      * @Route("/api/{query}", name="book_api")
      */
     public function index(string $query, HttpClientInterface $client): Response
