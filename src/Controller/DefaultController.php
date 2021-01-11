e1ed14ef542afa6f6f2c974c5994a41ac9a28e5c
Anders Björkland <git@andersbjorkland.online>
2021-01-11 12:20:42 +0100

    added outline for authentication

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\CurlHttpClient;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/auth", name="default")
+     */
+    public function index(string $query, HttpClientInterface $client): Response
+    {
+        return new Response("Yiipi!");
+    }
+}
